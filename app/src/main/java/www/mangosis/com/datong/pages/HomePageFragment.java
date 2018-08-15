1153fd6ca62d6ac582d0a85f8f9edcd079aa8d01
admin <yangxinbin@hotmail.com>
2018-08-15 11:53:33 +0800

    主页UI框架提交。

--- app/src/main/java/www/mangosis/com/datong/pages/HomePageFragment.java
+++ app/src/main/java/www/mangosis/com/datong/pages/HomePageFragment.java
@@ -3,25 +3,48 @@ package www.mangosis.com.datong.pages;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import org.greenrobot.eventbus.EventBus;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import www.mangosis.com.datong.R;
+import www.mangosis.com.datong.pages.adapter.HomePageAdapter;
 
 /**
  * Created by admin on 2018/8/14.
  */
 
 public class HomePageFragment extends Fragment {
+    @Bind(R.id.recycle)
+    RecyclerView recycle;
+    @Bind(R.id.refresh)
+    SmartRefreshLayout refresh;
+    HomePageAdapter homePageAdapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.homepage_fragment, container, false);
         ButterKnife.bind(this, view);
+        initView();
         return view;
+    }
+
+    private void initView() {
+        homePageAdapter = new HomePageAdapter(getActivity());
+        recycle.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
+        recycle.setAdapter(homePageAdapter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 }
