c9791a5f1587cb053ac3073f1acc496bf3b842b2
admin <yangxinbin@hotmail.com>
2018-08-20 17:48:35 +0800

    进入外卖

--- app/src/main/java/www/mangosis/com/datong/home/home/PlateFragment.java
+++ app/src/main/java/www/mangosis/com/datong/home/home/PlateFragment.java
@@ -19,45 +19,46 @@ import www.mangosis.com.datong.R;
 import www.mangosis.com.datong.waimai.WaiMaiActivity;
 
 
-public class PlateFragment extends Fragment
-{
+public class PlateFragment extends Fragment {
     GridView gridView;
 
-    int [] res = {
+    int[] res = {
             R.drawable.food,
             R.drawable.play,
             R.drawable.travel,
             R.drawable.shopping,
             R.drawable.supermarket,
             R.drawable.takeaway,};
-    String [] titles = {"享美食","享娱乐","享旅游","享购物","享农超","外卖"};
+    String[] titles = {"享美食", "享娱乐", "享旅游", "享购物", "享农超", "外卖"};
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
-    {
-        View view = inflater.inflate(R.layout.grid,container,false);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.grid, container, false);
         gridView = (GridView) view.findViewById(R.id.gridview);
         ArrayList<HashMap<String, Object>> item = new ArrayList<HashMap<String, Object>>();
-        for (int i = 0; i < res.length; i++)
-        {
+        for (int i = 0; i < res.length; i++) {
             HashMap<String, Object> map = new HashMap<String, Object>();
             map.put("itemImage", res[i]);
             map.put("itemName", titles[i]);
             item.add(map);
         }
 
-        SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(),item,R.layout.grid_content,
-                new String[]{"itemImage","itemName"},new int[]{R.id.image,R.id.text});
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(), item, R.layout.grid_content,
+                new String[]{"itemImage", "itemName"}, new int[]{R.id.image, R.id.text});
 
         gridView.setAdapter(simpleAdapter);
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //Toast.makeText(getContext(),titles[position], Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getActivity(), WaiMaiActivity.class);
-                startActivity(intent);
+                if (position == 5) {
+                    Intent intent = new Intent(getActivity(), WaiMaiActivity.class);
+                    startActivity(intent);
+                }
+
             }
         });
-        return  view;
+        return view;
     }
 }
